113274b52eded2329abc847a61802963e0e4a30b
Jonathan Rudenberg <jonathan@titanous.com>
2025-08-13 13:37:53 -0400

    Refactor to use ingress package for generated clients
    - Update protoc-gen-go-restate to generate ingress clients using ingress package
    - Remove dependency on client package (now deleted)
    - Generated clients now accept *ingress.Client instead of context
    - Context is passed per-request to Request() and Send() methods
    - Add WorkflowSubmitOption type for WithWorkflowRun option
    - Update codegen example to use new ingress API

--- examples/codegen/client/client.go
+++ examples/codegen/client/client.go
@@ -3,36 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
-	"github.com/restatedev/sdk-go/client"
+	"github.com/restatedev/sdk-go/ingress"
 	helloworld "github.com/restatedev/sdk-go/examples/codegen/proto"
 )
 
 func main() {
-	ctx, err := client.Connect(context.Background(), "http://127.0.0.1:8080")
-	if err != nil {
-		panic(err)
-	}
+	client := ingress.NewClient("http://127.0.0.1:8080")
 
-	greeter := helloworld.NewGreeterIngressClient(ctx)
-	greeting, err := greeter.SayHello().Request(&helloworld.HelloRequest{Name: "world"})
+	// Example with custom context (e.g., with timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	
+	greeter := helloworld.NewGreeterIngressClient(client)
+	greeting, err := greeter.SayHello().Request(ctx, &helloworld.HelloRequest{Name: "world"})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(greeting.Message)
 
-	workflow := helloworld.NewWorkflowIngressClient(ctx, "my-workflow")
-	send, err := workflow.Run().Send(&helloworld.RunRequest{})
+	workflow := helloworld.NewWorkflowIngressClient(client, "my-workflow")
+	invocation := workflow.Run().Send(context.Background(), &helloworld.RunRequest{})
+	
+	status, err := workflow.Status().Request(context.Background(), &helloworld.StatusRequest{})
 	if err != nil {
 		panic(err)
 	}
-	status, err := workflow.Status().Request(&helloworld.StatusRequest{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("workflow running with invocation id", send.InvocationId, "and status", status.Status)
+	fmt.Println("workflow running with invocation id", invocation.Id, "and status", status.Status)
 
-	if _, err := workflow.Finish().Request(&helloworld.FinishRequest{}); err != nil {
+	if _, err := workflow.Finish().Request(context.Background(), &helloworld.FinishRequest{}); err != nil {
 		panic(err)
 	}
 }
